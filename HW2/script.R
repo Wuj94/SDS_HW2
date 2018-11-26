578ee6c0ba27e126dc75d4d7fbe9b599787fd671
Giuseppe Calabrese <calabrese.giuseppe94@gmail.com>
2018-11-27 00:37:22 +0100

    asd

--- HW2/script.R
+++ HW2/script.R
@@ -1,29 +1,33 @@
-# 1 # retrieving informations 
-
-library('tseries')
-library('zoo')
-# 1) energy
-
-# Load the package
-require(tseries, quietly = TRUE)
-?get.hist.quote
-# Get Apple Inc. from NYSE
-
-energy.instr <- c('APA', 'BHGE', 'BC', 'COG', 'SNP', 'COP', 'CRT',
-                  'XEC', 'CHK','CVX')
+# Download the data -------------------------------------------------------
+require(tseries, quietly = T)
+require(data.table)
 
 
-for (i in 1:length(energy.instr)){
-  print(energy.instr[i])
-  aapl <- suppressWarnings(
-    get.hist.quote(instrument=energy.instr[i], start="2003-01-01", end="2008-01-01",
-                   quote= c("Open","Close"), provider="yahoo", drop=TRUE) )
-  # Take a look
-  class(aapl); names(aapl); head(aapl)
+sp.list <- fread('SPlist.csv')
+sp.list$Sector <- as.factor(sp.list$Sector)
+sectors <- levels(sp.list$Sector)
+
+get_series <- function(sp.list, sector, n = 10) {
+  n_series <- list()
+  energy.symbols <- sp.list[sp.list$Sector == sector]$Symbol
   
-  # Build some relative performance measure like: Close/Open
-  aapl$y <- aapl$Close/aapl$Open
-  # Plot
-  plot(aapl, main = energy.instr[i], xlab = "Year")
+  i <- 1
+  added <- 0
+  while(added < n && i <= length(energy.symbols)){
+    result = tryCatch({
+      a_series <- suppressWarnings(
+        get.hist.quote(instrument = energy.symbols[i], start = '2003-01-01', end = '2008-01-01',
+                       quote = c('Close'), provider = 'yahoo', drop = T) )
+      if(start(a_series) == '2003-01-01' && end(a_series) == '2007-12-31') {
+        n_series[[i]] <- a_series
+        added <- added + 1
+      }
+    }, error = function(e) {
+      print("error")
+    })
+    i <- i + 1
+  }
+  return(n_series)
 }
 
+series <- get_series(sp.list, sector = sectors[1])
