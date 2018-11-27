fdd043b899f701f4967bf45bbb1948a1944536b7
Giuseppe Calabrese <calabrese.giuseppe94@gmail.com>
2018-11-27 01:37:48 +0100

    project and others

--- HW2/script.R
+++ HW2/script.R
@@ -13,21 +13,34 @@ get_series <- function(sp.list, sector, n = 10) {
   
   i <- 1
   added <- 0
+  names.arr <- c()
   while(added < n && i <= length(energy.symbols)){
     result = tryCatch({
       a_series <- suppressWarnings(
         get.hist.quote(instrument = energy.symbols[i], start = '2003-01-01', end = '2008-01-01',
                        quote = c('Close'), provider = 'yahoo', drop = T) )
-      if(start(a_series) == '2003-01-01' && end(a_series) == '2007-12-31') {
-        n_series[[i]] <- a_series
+      if(start(a_series) == '2003-01-02' && end(a_series) == '2007-12-31') {
         added <- added + 1
+        n_series[[added]] <- a_series
+        names.arr <- c(names.arr, energy.symbols[i])
       }
     }, error = function(e) {
       print("error")
     })
     i <- i + 1
   }
+  names(n_series) <- names.arr
   return(n_series)
 }
 
-series <- get_series(sp.list, sector = sectors[1])
+data <- list()
+sector.names <- c()
+for(i in 1:length(sectors)){
+  data[[i]] <- get_series(sp.list, sector = sectors[i])
+  sector.names <- c(sector.names, sectors[i])
+}
+names(data) <- sector.names
+
+save(data, file='data.RData')
+
+load(file='data.RData')
