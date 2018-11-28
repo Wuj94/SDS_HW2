22db78012cecd7d706789501becdf2f3cb81f058
Giuseppe Calabrese <calabrese.giuseppe94@gmail.com>
2018-11-28 22:24:00 +0100

    functions bootstrap

--- HW2/functions.R
+++ HW2/functions.R
@@ -12,14 +12,49 @@ create_matrix <- function(sectors, data) {
   return(X)
 }
 
-bootstrap_proc <- function(X, corr_matrix, B = 1000, N = 1000){
-  delta_b  <- list()
-  for(i in B){
-    indices <- sample(1:nrow(X), N)
-    B_sample <- X[indices, ]
-    B_cor <- cor(B_sample, B_sample)
-    sim_stat <- sqrt(N) * max(abs(B_cor - corr_matrix))
-    delta_b[[i]] <- sim_stat
+bootstrap_procedure <- function(X, R_hat, B = 1000, n = 400){
+  delta_b <- c()
+  for(i in 1:B){
+    b_row_indices <- sample(1:nrow(X), n, replace = T)
+    b_sample <- X[b_row_indices, ]
+    b_R_hat <- cor(b_sample, b_sample)
+    delta_b <- c(delta_b, sqrt(n) * max(abs(b_R_hat - R_hat)))
   }
-  distro <- ecdf(delta_b)
+  
+  F_hat <- ecdf(delta_b)
+  alpha <- 0.05
+  t_alpha <- quantile(F_hat, 1-alpha)
+  low_conf_int <- R_hat - t_alpha / sqrt(n)
+  high_conf_int <- R_hat + t_alpha / sqrt(n)
+  
+  res <- list()
+  res[[1]] <- low_conf_int
+  res[[2]] <- high_conf_int
+  return(res)
+}
+
+get_series <- function(sp.list, sector, n = 10) {
+  n_series <- list()
+  energy.symbols <- sp.list[sp.list$Sector == sector]$Symbol
+  
+  i <- 1
+  added <- 0
+  names.arr <- c()
+  while(added < n && i <= length(energy.symbols)){
+    result = tryCatch({
+      a_series <- suppressWarnings(
+        get.hist.quote(instrument = energy.symbols[i], start = '2003-01-01', end = '2008-01-01',
+                       quote = c('Close'), provider = 'yahoo', drop = T) )
+      if(start(a_series) == '2003-01-02' && end(a_series) == '2007-12-31') {
+        added <- added + 1
+        n_series[[added]] <- a_series
+        names.arr <- c(names.arr, energy.symbols[i])
+      }
+    }, error = function(e) {
+      print("error")
+    })
+    i <- i + 1
+  }
+  names(n_series) <- names.arr
+  return(n_series)
 }
