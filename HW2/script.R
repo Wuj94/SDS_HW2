dca68da0d6dd818639ca8d67e3c4912f79f5afe9
Giuseppe Calabrese <calabrese.giuseppe94@gmail.com>
2018-12-03 10:34:46 +0100

    updated functions

--- HW2/script.R
+++ HW2/script.R
@@ -2,6 +2,7 @@
 require(tseries, quietly = T)
 require(data.table)
 library(zoo)
+source('functions.R')
 
 sp.list <- fread('SPlist.csv')
 sp.list$Sector <- as.factor(sp.list$Sector)
@@ -18,7 +19,7 @@ names(data) <- sector.names
 
 save(data, file='data_5.RData')
 
-load(file = 'data.RData')
+load(file = 'data_5.RData')
 # Create data matrix ------------------------------------------------------
 
 comp_name <- c()
@@ -26,6 +27,7 @@ for(li in data){
   comp_name <- c(comp_name, names(li[1:n_companies]))
 }
 
+X <- matrix()
 X <- create_matrix(sectors, data)
 colnames(X) <- comp_name
 
@@ -36,12 +38,15 @@ R_hat <- cor(X,X)
 
 conf_int <- bootstrap_procedure(X, R_hat = R_hat)
 
-epsilon <- .5
-edges <- ((conf_int[[1]] <= -epsilon) & (-epsilon <= conf_int[[2]])
-   | (conf_int[[1]] <= epsilon) & (epsilon <= conf_int[[2]]))
+epsilon <- .4
+edges <- ! (((conf_int[[1]] <= -epsilon) & (-epsilon <= conf_int[[2]])
+   | (conf_int[[1]] <= epsilon) & (epsilon <= conf_int[[2]])))
 
 require(igraph)
 g <- graph_from_adjacency_matrix(adjmatrix = edges, mode = c('undirected'), diag = 0)
+
+colors = c("green", "blue", "orchid", "orange", "yellow", "red", "antiquewhite", "chartreuse4", "cyan4", "darkgoldenrod3", "khaki")
+
 V(g)[1:5]$color <- "green"
 V(g)[6:10]$color <- "blue"
 V(g)[11:15]$color <- "orchid"
@@ -53,4 +58,37 @@ V(g)[36:40]$color <- "chartreuse4"
 V(g)[41:45]$color <- "cyan4"
 V(g)[46:50]$color <- "darkgoldenrod3"
 V(g)[51:55]$color <- "khaki"
-plot(g, rescale = F, ylim=c(-5, 5), xlim=c(-6,6) ,vertex.size=20, axes=T)
+layout <- layout.auto
+
+plot(g, vertex.size=15, layout=layout)
+
+# energy  -----------------------------------------------------------------
+
+# use energy::dcov.test and use a bonferroni threshold
+require(energy)
+
+M <- matrix(NA, ncol = ncol(X), nrow = ncol(X))
+for(i in 1:ncol(X)) {
+  for(j in 1:ncol(X)) {
+    dcor.i.j <- dcor.test(X[,i], X[,j], R=10)
+    M[i,j] <- dcor.i.j$p.value
+  }
+}
+
+# awand a jidd
+g <- graph_from_adjacency_matrix(adjmatrix = M < 0.2, mode = c('undirected'), diag = 0)
+
+V(g)[1:5]$color <- "green"
+V(g)[6:10]$color <- "blue"
+V(g)[11:15]$color <- "orchid"
+V(g)[16:20]$color <- "orange"
+V(g)[21:25]$color <- "yellow"
+V(g)[26:30]$color <- "red"
+V(g)[31:35]$color <- "antiquewhite"
+V(g)[36:40]$color <- "chartreuse4"
+V(g)[41:45]$color <- "cyan4"
+V(g)[46:50]$color <- "darkgoldenrod3"
+V(g)[51:55]$color <- "khaki"
+layout <- layout.auto
+
+plot(g, vertex.size=15, layout=layout)
