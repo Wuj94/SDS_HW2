041c5a119df1840fcfd9622521fe4999cc010e14
Giuseppe Calabrese <calabrese.giuseppe94@gmail.com>
2018-12-04 23:10:58 +0100

    updated functions

--- HW2/functions.R
+++ HW2/functions.R
@@ -14,23 +14,27 @@ create_matrix <- function(sectors, data) {
 
 bootstrap_procedure <- function(X, R_hat, B = 1000, n = 400, alpha = 0.05){
   delta_b <- c()
+  D <- ncol(X)
   for(i in 1:B){
     b_row_indices <- sample(1:nrow(X), n, replace = T)
     b_sample <- X[b_row_indices, ]
     b_R_hat <- cor(b_sample, b_sample)
-    delta_b <- c(delta_b, sqrt(n) * max(abs(b_R_hat - R_hat)))
+    delta_b <- c(delta_b, sqrt(D) * max(abs(b_R_hat - R_hat)))
   }
   
   F_hat <- ecdf(delta_b)
   t_alpha <- quantile(F_hat, 1-alpha)
-  low_conf_int <- R_hat - t_alpha / sqrt(n)
-  high_conf_int <- R_hat + t_alpha / sqrt(n)
+  low_conf_int <- R_hat - t_alpha / sqrt(D *(D-1) / 2) # put here D 
+  high_conf_int <- R_hat + t_alpha / sqrt(D *(D-1) / 2) 
   
   res <- list()
   res[[1]] <- low_conf_int
   res[[2]] <- high_conf_int
   return(res)
 }
+
+
+# 1. get data
 
 get_series <- function(sp.list, sector, n = 10) {
   n_series <- list()
@@ -57,3 +61,46 @@ get_series <- function(sp.list, sector, n = 10) {
   names(n_series) <- names.arr
   return(n_series)
 }
+
+
+# 1.b get random data
+
+
+get.random.series <- function(sp.list, sector, n = 10) {
+
+  #set.seed(1234)
+  n_series <- list()
+  
+  sector.symbols <- sp.list[sp.list$Sector == sector]$Symbol
+  
+  i = 0
+  cnt = 0
+  symbols.list <- c()
+  names.arr <- c()
+  while ((i < n) & (cnt < length(sector.symbols))){
+    
+    random.sym = sample(sector.symbols, size=1)
+    
+    result = tryCatch({
+      if (!(random.sym %in% symbols.list)){
+        symbols.list <- c(symbols.list, random.sym)
+        cnt <- cnt + 1
+        a_series <- suppressWarnings(
+          get.hist.quote(instrument = random.sym, start = '2003-01-01', end = '2008-01-01',
+                         quote = c('Close'), provider = 'yahoo', drop = T) )
+        if((start(a_series) == '2003-01-02') && (end(a_series) == '2007-12-31')) {
+          n_series[[i+1]] <- a_series
+          names.arr <- c(names.arr, random.sym)
+          i <- i + 1
+        }
+      }
+    }, error = function(e) {
+      print(e)
+    })
+
+  }
+  
+  names(n_series) <- names.arr
+  return (n_series)
+}
+  
