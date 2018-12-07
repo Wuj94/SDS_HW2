f10d1a289a9fcd2d862d0a57fa8d9c39e70ada04
cerniello <43636687+cerniello@users.noreply.github.com>
2018-12-07 17:44:41 +0100

    uploads

--- HW2/functions.R
+++ HW2/functions.R
@@ -12,17 +12,42 @@ create_matrix <- function(sectors, data) {
   return(X)
 }
 
-bootstrap_procedure <- function(X, R_hat, B = 1000, n = 400, alpha = 0.05){
+# implements the bootstrap procedure described in the notes
+# input parameters:
+# - X: data matrix (D stocks and n observation)
+# - B: number of bootstrap replications
+# - n: size of the samples (rows picked randomly from X)
+# - alpha: level of the confidence intervals
+# output: 
+# -list containing two matrices:
+##  [0]: lower bounds of the CI's
+##  [0]: upper bounds of the CI's
+
+bootstrap_procedure <- function(X, B = 1000, n = 400, alpha = 0.05){
+  
+  # sample correlation matrix
+  R_hat <- cor(X,X)
+  
+  # bootstrapped replicates of simultaneous test statistics
   delta_b <- c()
+  
+  # bootstrap procedure
   for(i in 1:B){
+    # matrix sampled from X
     b_row_indices <- sample(1:nrow(X), n, replace = T)
     b_sample <- X[b_row_indices, ]
+    
+    # bootstrap correlation matrix
     b_R_hat <- cor(b_sample, b_sample)
     delta_b <- c(delta_b, sqrt(ncol(X)) * max(abs(b_R_hat - R_hat)))
   }
   
+  # ECDF of the statistics
   F_hat <- ecdf(delta_b)
+  # sample quantile (at level 1-alpha)
   t_alpha <- quantile(F_hat, 1-alpha)
+  
+  # lower and upper bounds
   low_conf_int <- R_hat - t_alpha / sqrt(ncol(X)) 
   high_conf_int <- R_hat + t_alpha / sqrt(ncol(X)) 
   
@@ -33,7 +58,8 @@ bootstrap_procedure <- function(X, R_hat, B = 1000, n = 400, alpha = 0.05){
 }
 
 # plot graph
-plot.graph <- function(edges, nstock = 3, nsectors = 11 ){
+plot.graph <- function(edges, nstock = 3, sectors){
+  #nsectors <- length(sectors)
   g <- graph_from_adjacency_matrix(adjmatrix = edges, mode = c('undirected'), diag = 0)
   
   colors = c("green", "blue", "orchid", "orange", "yellow", "antiquewhite",
@@ -41,21 +67,26 @@ plot.graph <- function(edges, nstock = 3, nsectors = 11 ){
   
   layout <- layout.auto
   
-  V(g)[1:nstock]$color <- colors[1]
+  V(g)$color = rep(colors, each=nstock)
+ 
+  plot(g, edges.arrow.size = 5, vertex.size=20, vertex.label.dist = 2,
+       layout=layout, edge.width = 1.5)
   
-  for (i in 2:nsectors){
-      low <- (i-1)*nstock+1
-      up <- i*nstock
-      V(g)[low:up]$color <- colors[i]
-  }
+  legend(x = 'topleft', sectors, col=colors, bty = 'n', cex=0.6, pch=16)
   
-  plot(g, vertex.size=25, layout=layout)
+#  legend(x=-1.5, y=-1.1, c("Newspaper","Television", "Online News"), pch=21,
+#         col="#777777", pt.bg=colrs, pt.cex=2, cex=.8, bty="n", ncol=1)
+  
   
 }
 
 
 
 # 1. get data
+
+# 1. Get data -------------------------------------------------------------
+
+
 
 get_series <- function(sp.list, sector, n = 10) {
   n_series <- list()
@@ -87,12 +118,10 @@ get_series <- function(sp.list, sector, n = 10) {
 # 1.b get random data
 
 
-get.random.series <- function(sp.list, sector, n = 10) {
+get.random.series <- function(sector.symbols, n = 10) {
 
-  #set.seed(1234)
+  set.seed(1234)
   n_series <- list()
-  
-  sector.symbols <- sp.list[sp.list$Sector == sector]$Symbol
   
   i = 0
   cnt = 0
--- HW2/script.R
+++ HW2/script.R
@@ -1,58 +1,68 @@
 # Download the data -------------------------------------------------------
+
 require(tseries, quietly = T)
 require(data.table)
 library(zoo)
 source('functions.R')
 require(igraph)
 
+# read csv containing companies with related symbols and sectors
 sp.list <- fread('SPlist.csv')
 sp.list$Sector <- as.factor(sp.list$Sector)
+# pick the sectors names
 sectors <- levels(sp.list$Sector)
 
+# pick the quotes randomly (3 companies per sector)
 n_companies <- 3
 data <- list()
 sector.names <- c()
 for(i in 1:length(sectors)){
-  data[[i]] <- get.random.series(sp.list, sector = sectors[i], n = n_companies)
+  sector.symbols <- sp.list[sp.list$Sector == sectors[i]]$Symbol
+  data[[i]] <- get.random.series(sector.symbols, n = n_companies)
   sector.names <- c(sector.names, sectors[i])
 }
 
 names(data) <- sector.names
 
 save(data, file='data_3.RData')
+#load(file = 'data_3.RData')
 
-load(file = 'data_3.RData')
+
 # Create data matrix ------------------------------------------------------
 
-comp_name <- c()
+comp_symbols <- c()
 for(li in data){
-  comp_name <- c(comp_name, names(li[1:n_companies]))
+  comp_symbols <- c(comp_symbols, names(li[1:n_companies]))
 }
 
 X <- matrix()
 X <- create_matrix(sectors, data)
-colnames(X) <- comp_name
+colnames(X) <- comp_symbols
+
+X[1:5,1:5]
 # Create MC graph ---------------------------------------------------------
 
 # create pearson correlation matrix
-R_hat <- cor(X,X)
 
-conf_int <- bootstrap_procedure(X, R_hat = R_hat)
+conf_int <- bootstrap_procedure(X)
 
-epsilon <- c(.05, .1, .2, .3, .4)
+epsilon <- c(.1, .2, .3, .4)
 edges <- list()
 for(i in 1:length(epsilon)){
   edges[[i]] <-  !(((conf_int[[1]] <= -epsilon[i]) & (-epsilon[i] <= conf_int[[2]])
                | (conf_int[[1]] <= epsilon[i]) & (epsilon[i] <= conf_int[[2]])))
 }
 
-plot.graph(edges[[1]], nstock = n_companies, nsectors = length(sectors))
+plot.graph(edges[[1]], nstock = n_companies, sectors=sectors)
 
-plot.graph(edges[[2]])
+plot.graph(edges[[2]], nstock = n_companies, sectors=sectors)
 
-plot.graph(edges[[3]])
+plot.graph(edges[[3]], nstock = n_companies, sectors=sectors)
 
-plot.graph(edges[[4]])
+plot.graph(edges[[4]], nstock = n_companies, sectors = sectors)
+
+
+#plot.graph(edges[[4]], nstock = n_companies, nsectors = length(sectors))
 # energy  -----------------------------------------------------------------
 
 # use energy::dcov.test and use a bonferroni threshold
@@ -61,15 +71,15 @@ require(energy)
 M <- matrix(NA, ncol = ncol(X), nrow = ncol(X))
 diag(M) <- 1
 for(i in 1:(ncol(X)-1)) {
+  print(i)
   for(j in (i+1):ncol(X)) {
-    dcov.i.j <- dcov.test(X[,i], X[,j], index=0.001, R=200) # nina said : enlarge R and shrink index ( ~ 0.001 )
+    dcov.i.j <- dcov.test(X[,i], X[,j], index=0.001, R=1000) # nina said : enlarge R and shrink index ( ~ 0.001 )
     # Francesco said: provare con dcov.test
     M[i,j] <- M[j,i] <- dcov.i.j$p.value
   }
-  print(i)
 }
 
-save(M, file='dcovMatrix.Rdata')
+save(M, file='dcovMatrix_5.Rdata')
 dimnames(M) <- list(colnames(X),colnames(X))
 
 ?dcor.test
@@ -93,3 +103,4 @@ dimnames(bonf.test) <- dimnames(M)
 #g <- graph_from_adjacency_matrix(adjmatrix = alpha.test, mode = c('undirected'), diag = 0)
 
 plot.graph(bonf.test, nstock = n_companies, nsectors = length(sectors))
+
